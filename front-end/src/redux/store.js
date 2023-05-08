7d6e17be4cffa9507f36d9a5050742df5c14257a
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-05-08 13:07:36 -0300

    chore: configura redux-persist

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,13 +1,32 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import rootReducer from './reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(
+//     applyMiddleware(thunk),
+//   ),
+// );
+
 const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-  ),
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export {
+  store,
+  persistor,
+};
