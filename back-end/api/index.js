6dbba489c63815587d189144d2d631f413797805
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-08-29 16:36:53 -0300

    refactor: alterações para nao quebrar a main

--- back-end/api/index.js
+++
@@ -1,3 +0,0 @@
-import app from '../src/api/app';
-
-export default app;
--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,55 +1,24 @@
 require('dotenv').config();
-// const mysql2 = require('mysql2');
-
-// const environment = process.env.NODE_ENV || 'test';
-
-// const suffix = {
-//   prod: '',
-//   production: '',
-//   dev: '-dev',
-//   development: '-dev',
-//   test: '-test',
-// };
 
 const options = {
   use_env_variable: process.env.USE_ENV_VARIABLE || null,
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  // database: 
-  //   `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
   database: process.env.MYSQL_DB_NAME,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
-  // ssl: true,
   dialectOptions: {
     timezone: 'Z',
-    ssl: {
-      rejectUnauthorized: true,        
-    },
   },
   logging: false,
 };
 
-// if (options.dialect === 'mysql') {
-//   options.dialectModule = mysql2;
-// }
-
-// const options = {
-//   use_env_variable: "DATABASE_URL",
-//   dialect: "mysql",
-//   ssl: true,
-//   dialectOptions: {
-//       ssl: {
-//           rejectUnauthorized: true,        
-//       }
-//   }
-// }
-
-// console.log(options);
-// console.log(process.env.HOSTNAME);
-// console.log(process.env.MYSQL_HOST);
-// console.log(process.env.MYSQL_DB_NAME);
+if (options.use_env_variable) {
+  options.dialectOptions.ssl = {
+    rejectUnauthorized: false,
+  };
+}
 
 module.exports = {
   development: {
