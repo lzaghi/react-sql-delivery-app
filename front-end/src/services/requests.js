e5e8949cce181014c32521b5a3af5a0903c8eea4
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-08-30 14:57:29 -0300

    refactor: env variable

--- front-end/src/services/requests.js
+++ front-end/src/services/requests.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 
 const api = axios.create({
   // baseURL: `http://${process.env.REACT_APP_HOSTNAME || 'localhost'}:${process.env.REACT_APP_API_PORT || '3001'}`,
-  baseURL: process.env.PUBLIC_URL ?? 'localhost:3001',
+  baseURL: process.env.REACT_APP_PUBLIC_API ?? 'localhost:3001',
 });
-
+console.log(process.env.REACT_APP_PUBLIC_API);
 export const requestPost = async (endpoint, body) => {
   const { data } = await api.post(endpoint, body);
   return data;
